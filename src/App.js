ec4104efb178d9ba2cdc9b43f3ad0360d23494f6
AlokSharma01 <aloksj788@gmail.com>
2022-03-02 22:02:33 +0530

    subsciption plan

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Page from './component/nda/Page';
+import { Subscription } from './component/subscription-plan/Subscription';
 
 function App() {
   return (
     <div className="App">
-      <Page/>
+      {/* <Page/> */}
+      <Subscription/>
     </div>
   );
 }
